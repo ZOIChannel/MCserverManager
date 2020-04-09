39028422523391a457e0f63f1802123340b86bbf
ZOI <zoizoichannel@gmail.com>
2020-04-09 17:57:28 +0900

    electron ready.

---
+++ build-mac.js
@@ -0,0 +1,12 @@
+
+const builder = require('electron-builder');
+
+builder.build({
+    config: {
+        'appId': 'appId': 'local.test.app1',,
+        'mac':{
+            'target': 'zip',
+        }
+    }
+});
+
---
+++ build.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd `dirname $0`
+node build-win
+node build-mac
