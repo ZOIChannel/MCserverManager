01302cf1d463867acf210861c351db5cefa2a06d
ZOI <zoizoichannel@gmail.com>
2020-04-10 00:05:45 +0900

    this code can run web server at 3500.

--- main.js
+++ main.js
@@ -4,10 +4,21 @@ const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 app.on('ready', () => {
+  // webServer
+  const webProc = require('child_process');
+  // サーバーの実行
+  var server = webProc.spawn(
+    "node",
+    ['server.js'],
+    { cwd: __dirname } // サーバーファイルのディレクトリ
+  );
+
+
   // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
   mainWindow = new BrowserWindow({ width: 400, height: 300 });
   // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  // mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('http://localhost:3500');
 
   // ChromiumのDevツールを開く
   mainWindow.webContents.openDevTools();
