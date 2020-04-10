0091eb4c18da8de6a85fa73285a21a1fb0f0fe82
ZOI <51259731+ZOIChannel@users.noreply.github.com>
2020-04-10 13:06:41 +0900

    Update build-mac.js

--- build-mac.js
+++ build-mac.js
@@ -3,9 +3,9 @@ const builder = require('electron-builder');
 
 builder.build({
     config: {
-        'appId': 'appId': 'local.test.app1',,
+        'appId': 'local.test.app1',
         'mac':{
-            'target': 'zip',
+            'target': 'zip'
         }
     }
 });
