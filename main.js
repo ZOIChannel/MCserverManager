7031dab91bceef86c0105f985e75f40efa0bb2ba
ZOI-dayo <zoi@zoizoi.net>
2020-07-09 17:56:20 +0900

    Make Window

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ app.on('ready', () => {
 
 
     // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
-    mainWindow = new BrowserWindow({ width: 400, height: 300 });
+    mainWindow = new BrowserWindow({ width: 1280, height: 720 });
     // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     // mainWindow.loadURL('http://localhost:3500');
 
     // ChromiumのDevツールを開く
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', function() {
         mainWindow = null;
